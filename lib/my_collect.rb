39cd06401d1eb9d51cb2d7e71552b85bee04274e
johncban <johncban@gmail.com>
2019-04-22 08:46:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,15 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-def my_collect()
+def my_collect(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+end
 
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
