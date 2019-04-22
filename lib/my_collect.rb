cc37cefc9ff00e26e36efa0e20bade08073c9847
johncban <johncban@gmail.com>
2019-04-22 08:51:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,12 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
   i = 0
   collection = []
+
   while i < array.length
     collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 my_collect(array) do |name|
